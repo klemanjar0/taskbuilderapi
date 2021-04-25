e9f4ea5ca963d6fcc679e2f40081e2df0e6bf02d
klemanjar0 <klim2838@gmail.com>
2021-04-25 19:53:44 +0300

    v0.4.2

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,10 @@ const users = require('./users');
 const folders = require('./folders');
 const notes = require('./notes');
 
+router.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
 router.use('/user', users);
 router.use('/folder', folders);
 router.use('/note', notes);
